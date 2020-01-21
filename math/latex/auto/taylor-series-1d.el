93471af880afc69cc3fddd2f6e38b8a440a3f1b3
john-waczak <john.louis.waczak@gmail.com>
2020-01-21 12:26:12 -0600

    reorganized and added in thermo

---
+++ math/latex/auto/taylor-series-1d.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook
+ "taylor-series-1d"
+ (lambda ()
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "amsmath"
+    "cancel"))
+ :latex)
+
---
+++ physics/classical-mechanics/latex/auto/degrees-of-freedom.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook
+ "degrees-of-freedom"
+ (lambda ()
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "amsmath"
+    "cancel"))
+ :latex)
+
